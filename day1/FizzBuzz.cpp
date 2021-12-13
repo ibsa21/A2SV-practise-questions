9384c87f5eff2ba805fc005e68439aa7486048bd
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2021-12-13 17:38:08 +0300

    Update FizzBuzz.cpp

--- day1/FizzBuzz.cpp
+++ day1/FizzBuzz.cpp
@@ -1,2 +1,22 @@
-include <iostream>
-/* this is my workspace*/
+
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string>answer;
+        if(n >=1 && n<=10000 )
+        {
+            for(int i = 1; i <= n; i++) {
+                if(i%3 == 0 && i%5==0) {
+                    answer.push_back("FizzBuzz");
+                } else if (i%3==0) {
+                    answer.push_back("Fizz");
+                }else if (i%5 == 0) {
+                    answer.push_back("Buzz");
+                } else {
+                    answer.push_back(to_string(i));
+                }
+        }
+        }
+        return answer;
+    }
+};
