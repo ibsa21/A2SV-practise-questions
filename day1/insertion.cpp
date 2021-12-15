b743680aa6dec225cb5043749d998134577cb374
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2021-12-15 15:26:41 +0300

    Update insertion.cpp

--- day1/insertion.cpp
+++ day1/insertion.cpp
@@ -14,18 +14,9 @@ vector<string> split(const string &);
  *  2. INTEGER_ARRAY arr
  */
 
-int insertionSort1(int n, vector<int> arr) {
-    if(n < 0 || n > 10000) {
-        return false;
-    }
-    for(int i = 0; i < arr.size(); i++) {
-        if(arr[i] > 10000 || arr[i] < -10000) {
-            return false;
-        }
-    }
-    
+void insertionSort1(int n, vector<int> arr) {
     int number = arr[n-1];
-    for(int i = n-2; i >=-1; i--) {
+    for(int i = n-2; i >=0; i--) {
         if(number < arr[i]) {
             arr[i+1] = arr[i];
             for(int k = 0; k < n; k++) {
@@ -34,13 +25,11 @@ int insertionSort1(int n, vector<int> arr) {
             cout << endl;
         } else {
             arr[i + 1] = number;
-            break;
         }
     }
                 for(int k = 0; k < n; k++) {
                 cout << arr[k] << " ";
             }
-    return 0;
     
 }
 
@@ -107,4 +96,3 @@ vector<string> split(const string &str) {
 
     return tokens;
 }
-// end of the code
