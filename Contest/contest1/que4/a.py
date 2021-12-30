c1a06982ab5d99ff2d1fbe3ec431cdaab91e4b30
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2021-12-30 23:04:12 +0300

    Create a.py

---
+++ Contest/contest1/que4/a.py
@@ -0,0 +1,17 @@
+rows = 3
+for i in range(1, rows + 1):
+    print(" " * (rows - i), end=" ")
+    for j in range(1, i + 1):
+        print(j, end="")
+    for k in list(reversed(range(1, i))):
+        print(k, end="")
+    print()
+
+# second for loop to print the lower section of the diamond
+for i in range(rows -1, 0, -1):
+    print(" " * (rows - i), end=" ")
+    for j in range(1, i + 1):
+        print(j, end="")
+    for k in list(reversed(range(1, i))):
+        print(k, end="")
+    print()
