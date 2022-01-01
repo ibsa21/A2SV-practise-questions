814c4f9e601fd9e5b5af3a7f74ef60bfa4da6fbf
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-01-01 10:37:41 +0300

    Update merge.py

--- week 3/merge.py
+++ week 3/merge.py
@@ -2,7 +2,6 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort(key = lambda a:a[0])
         mediumInterval  = intervals[0]
-        print(mediumInterval)
         merged = []
         for i in intervals:
             if i[0] <= mediumInterval[1]:
