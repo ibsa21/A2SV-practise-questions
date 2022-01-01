37257d68ba80ef794d589f4e1bdc6bc1a6abfb03
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-01-01 09:25:16 +0300

    Update 5b.py

--- Contest/contest1/5b.py
+++ Contest/contest1/5b.py
@@ -1,8 +1,9 @@
 number = int(input("Enter number: "))
 string = []
-while(number != 0):
+while(number !=0 ):
+    number-= 1
     remainder = number % 26
-    string.append(chr(remainder + 64))
-    number = number // 16
-
+    string.append(chr(remainder + 65))
+    number = number // 26
+    
 print("".join(reversed(string)))
