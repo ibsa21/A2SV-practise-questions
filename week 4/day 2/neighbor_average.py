bde1ddb4286ec11e603ade12f2db0156133477a5
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-01-04 23:41:47 +0300

    Update neighbor_average.py

--- week 4/day 2/neighbor_average.py
+++ week 4/day 2/neighbor_average.py
@@ -3,13 +3,14 @@ class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
         nums.sort()
         median = statistics.median(nums)
-        odd = [x for x in range(1, len(nums), 2)]
-        even = [x for x in range(0, len(nums), 2)]
+        length  = len(nums)
+        odd = range(1, length, 2)
+        even = range(0, length, 2)
         
-        num = [0] * len(nums)
+        num = [0] * length
         j =0
         k = 0
-        for i in range(len(nums)):
+        for i in range(length):
             if (nums[i] < median):
                 print(odd[j], j)
                 num[odd[j]] = nums[i]
