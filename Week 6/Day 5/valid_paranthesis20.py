1388b6dbf00f8270a0eb61c8c38c58b819e6dc7e
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-01-13 15:49:42 +0300

    Create valid_paranthesis20.py

---
+++ Week 6/Day 5/valid_paranthesis20.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        dict = {"]":"[", "}":"{", ")":"("}
+        for char in s:
+            if char in dict.values():
+                stack.append(char)
+            elif char in dict.keys():
+                if stack == [] or dict[char] != stack.pop():
+                    return False
+        return stack == []
