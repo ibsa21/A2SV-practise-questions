0b8269ae9fbdf3b04c88aa2243ff55b420405a6f
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-01-24 14:00:19 +0300

    Create recent_calls.py

---
+++ week 7/recent_calls.py
@@ -0,0 +1,20 @@
+class RecentCounter:
+
+    def __init__(self):
+        self.counter = deque()
+        
+        
+    def ping(self, t: int) -> int:
+        self.counter.append(t)
+        
+        while self.counter[-1] - self.counter[0] > 3000:
+            self.counter.popleft()
+        
+        return len(self.counter);
+        
+        
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
