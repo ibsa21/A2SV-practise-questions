53bd9bc4571e7ca270560666875f4a9747f92b57
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-01-26 20:51:19 +0300

    Update reverse_string_344.py

--- week 7/reverse_string_344.py
+++ week 7/reverse_string_344.py
@@ -1,3 +1,4 @@
+#first approach using iterative method
 class Solution:
     def reverseString(self, s: List[str]) -> None:
         """
@@ -11,6 +12,23 @@ class Solution:
             s[left], s[right] = s[right], s[left]
             left +=1
             right -=1
+          
+#second approach using recursive method
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        self.reverse_string(s, 0, len(s)-1)
+        
+    def reverse_string(self, s, left, right):
+        if left > right:
+            return
+        else:
+            s[left], s[right] = s[right], s[left]
+            self.reverse_string(s, left + 1, right -1)
+        
+        
             
         
         
