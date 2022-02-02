7126604cb6c26cf2282b383bdd3cc9d817c6187b
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-02-03 02:38:01 +0300

    Create 50Pow(x, n).py

---
+++ Week 8/50Pow(x, n).py
@@ -0,0 +1,15 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        
+        if n == 0:
+            return 1
+        
+        if n < 0:
+            n=abs(n)
+            x=1/x
+            
+        if n%2==0:
+            a=self.myPow(x,n/2)
+            return a*a
+        
+        return x*self.myPow(x,n-1)
