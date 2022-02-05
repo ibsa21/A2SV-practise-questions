abcbc7cc811218760339d3f8c56f0ecc84389d35
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-02-05 18:11:05 +0300

    Create mark_and_toy.py

---
+++ Contest/Contest3/mark_and_toy.py
@@ -0,0 +1,54 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'maximumToys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY prices
+#  2. INTEGER k
+#
+
+def maximumToys(prices, k):
+    # Write your code here
+    prices.sort()
+    max_toy = 0
+    left = 0
+    sumValue = 0
+    
+    for right in range(len(prices)):
+        sumValue += prices[right]
+        
+        while sumValue > k:
+            left += 1
+            sumValue -= prices[left]
+        
+        max_toy = max(max_toy, right-left + 1)
+    
+    return max_toy
+        
+        
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    prices = list(map(int, input().rstrip().split()))
+
+    result = maximumToys(prices, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
