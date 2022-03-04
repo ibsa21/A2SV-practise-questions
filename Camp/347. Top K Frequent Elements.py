00f9933a0a62c272b6d27f49c0ae1d33614dc195
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-03-04 09:29:21 +0300

    Create 347. Top K Frequent Elements.py

---
+++ Camp/347. Top K Frequent Elements.py
@@ -0,0 +1,16 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        count_nums = Counter(nums)
+        value_key = []
+        
+        for key in count_nums:
+            value_key.append((count_nums[key], key))
+        
+        heapq._heapify_max(value_key)
+        
+        ans = []
+        for i in range(k):
+            ans.append(heapq._heappop_max(value_key)[1])
+        
+        return ans
