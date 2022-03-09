a2c3a65166e00072acef2fe137c0198e56d333e3
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-03-09 11:49:03 +0300

    Time: 216 ms (77.93%), Space: 14.5 MB (51.12%) - LeetHub

---
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        row, col = map(len, (isConnected, isConnected[0]))
+        
+        adjecancy_list = {}
+        
+        for i in range(row):
+            adjecancy_list[i] = []
+            for j in range(col):
+                if isConnected[i][j] == 1 and i !=j:
+                    adjecancy_list[i].append(j)
+        
+        visited = set()
+        
+        def dfs(node):
+            visited.add(node)    
+            for n in adjecancy_list[node]:
+                if n not in visited:
+                    dfs(n)
+        
+        res = 0
+        for i in range(row):
+            if i not in visited:
+                dfs(i)
+                res += 1
+        return res
+                
