9c8f61f3ba6b9cc234a9157a92ec3cde9103c6e7
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-03-10 10:17:20 +0300

    Time: 655 ms (9.78%), Space: 21.9 MB (19.19%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.py
@@ -0,0 +1,50 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        
+        row_len, col_len= map(len, (grid, grid[0]))
+        directions = [(-1,0), (1, 0), (0, 1), (0, -1)]
+        
+        queue = collections.deque()
+        
+        no_islands = 0            
+        visited = set()
+        
+        def isValid(r, c):
+            return (0<=r<row_len and 0 <= c < col_len) and (grid[r][c]=="1" and (r, c) not in visited)
+    
+        #print(queue)
+        
+        def bfs(row, col):
+            visited.add((row, col))
+            queue.append((row, col))
+            
+            while queue:
+
+                row, col = queue.popleft()
+                for r, c in directions:
+                    new_row = row + r
+                    new_col =  col + c
+
+                    if isValid(new_row, new_col):
+                        visited.add((new_row, new_col))
+                        queue.append((new_row, new_col))
+        
+        for i in range(row_len):
+            for j in range(col_len):
+                if grid[i][j]=="1" and (i, j) not in visited:
+                    bfs(i, j)
+                    no_islands += 1
+                
+        return no_islands
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+            
+            
+        
