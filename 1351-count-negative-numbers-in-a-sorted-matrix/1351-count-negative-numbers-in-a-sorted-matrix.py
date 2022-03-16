8543c4aeeaf288e1d9421fb76428b2efbb4c6270
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-03-16 15:14:00 +0300

    Update 1351-count-negative-numbers-in-a-sorted-matrix.py

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.py
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.py
@@ -1,3 +1,4 @@
+#approach 1 - binary search O(n + m)
 class Solution:
     def countNegatives(self, grid: List[List[int]]) -> int:
         
@@ -22,4 +23,38 @@ class Solution:
            binary_search(0, len(arr)-1, arr)
         
         return best
+ 
+
+ #approach number 2 - depth first search graph traversal
+ #O(n) - time complexity approach
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+        visited = set()
+        count_negative = 0
+        
+        def isValid(r, c):
+            return (0<= r < len(grid) and 0<=c<len(grid[0])) and ((r,c) not in visited and grid[r][c] < 0)
+        
+        def dfs(row, col):
+            
+            nonlocal count_negative
+            for i, j in directions:
+                new_row = row + i
+                new_col = col + j
+                
+                if isValid(new_row, new_col):
+                    visited.add((new_row, new_col))
+                    count_negative += 1
+                    dfs(new_row, new_col)
+                    
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                
+                if grid[i][j] < 0 and (i, j) not in visited:
+                    count_negative +=1
+                    visited.add((i,j))
+                    dfs(i, j)
+                               
+        return count_negative
             
