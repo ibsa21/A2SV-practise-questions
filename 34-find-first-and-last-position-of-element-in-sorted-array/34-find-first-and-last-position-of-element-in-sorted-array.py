2d7b4499a515f81e3da4d02f496e125c4fd1e7a5
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-03-16 14:29:12 +0300

    Time: 88 ms (88.87%), Space: 15.5 MB (55.41%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -4,10 +4,9 @@ class Solution:
         best = -1
         
         def binary_search(left, right, is_first):
+            
             nonlocal best
             mid = left + (right-left)//2
-            
-            
             if left > right:
                 return -1 if best==-1 else best
             
@@ -27,7 +26,6 @@ class Solution:
         left = 0
         right = len(nums)-1
         first_index = binary_search(left, right, True)
-        print(first_index)
         
         if first_index == -1:
             return [-1 ,-1]
