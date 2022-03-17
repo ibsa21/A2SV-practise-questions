83fb12aa16a700346b1b73566f94fc11904b68a7
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-03-17 11:07:18 +0300

    Time: 187 ms (22.27%), Space: 13.9 MB (93.90%) - LeetHub

--- 1237-find-positive-integer-solution-for-a-given-equation/1237-find-positive-integer-solution-for-a-given-equation.py
+++ 1237-find-positive-integer-solution-for-a-given-equation/1237-find-positive-integer-solution-for-a-given-equation.py
@@ -11,29 +11,19 @@
 
 class Solution:
     def findSolution(self, customfunction: 'CustomFunction', z: int) -> List[List[int]]:
-        
         res = []
-        
-        def binary_search(x, low, high):
-            
-            if low > high:
-                return -1
-            
-            mid = low + (high-low)//2
-            
-            temp_result = customfunction.f(x, mid)
-            
-            if temp_result > z:
-                binary_search(x, low, mid-1)
-            elif temp_result < z:
-                binary_search(x, mid + 1, high)
-            else:
-                res.append([x, mid])
-        
-        x  = 1
-        y = 1000
-        for i in range(1, 1000):
-            binary_search(i, x, y)
-            
+        for i in range(1,1000):
+
+            left, right = 1, 1000
+            while left <= right:
+
+                mid  = left + (right - left)//2
+                temp = customfunction.f(i, mid)
+                if temp == z:
+                    res.append([i, mid])
+                    break
+                elif temp > z:
+                    right = mid - 1
+                elif temp < z:
+                    left = mid + 1
         return res
-        
