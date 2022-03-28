9dfae6c71ddd7a21ed1c560e19196215bfe36536
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-03-28 21:22:41 +0300

    Time: 125 ms (86.88%), Space: 14 MB (23.46%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -13,32 +13,28 @@ class Solution:
                     max_freq = char_freq[key]
             return max_freq
         
-        def sum_charFreq():
-            sum_value = 0
-            
-            for key in char_freq:
-                sum_value += char_freq[key]
-                
-            return sum_value
         
         max_len = 0
         left = 0
         right = 0
-        k_count = k
         
+        sum_frequency  = 0
+        max_curr_freq = 0
         while right < len(s):
             
             char_freq[s[right]] += 1
+            sum_frequency += 1
             
-            if sum_charFreq() - find_maxChar() <= k:
+            if char_freq[s[right]] >= max_curr_freq:
+                max_curr_freq = char_freq[s[right]]
+                
+            if sum_frequency - max_curr_freq <= k:
                 max_len = max(max_len, right - left + 1)
             else:
                 char_freq[s[left]] -= 1
+                sum_frequency -= 1
                 left += 1
             
             right +=1
         
         return max_len
-                
-            
-            
