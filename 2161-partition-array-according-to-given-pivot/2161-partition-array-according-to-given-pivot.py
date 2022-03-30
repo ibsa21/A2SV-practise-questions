810e60704d9a72fcfb014445a89996056678e123
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-03-30 18:34:14 +0300

    Update 2161-partition-array-according-to-given-pivot.py

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
@@ -1,3 +1,4 @@
+#first approach
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
         
@@ -28,3 +29,26 @@ class Solution:
                 result[p_index] = num 
                 p_index += 1
         return result
+
+    #second approach
+    class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        
+        lessthan = []
+        greaterthan  = []
+        equal_pivot = []
+        
+        for num in nums:
+            if num < pivot:
+                lessthan.append(num)
+                
+            elif num > pivot:
+                greaterthan.append(num)
+            else:
+                
+                equal_pivot.append(num)
+        
+        lessthan.extend(equal_pivot)
+        lessthan.extend(greaterthan)
+        
+        return lessthan
