5d7fe5fc802ed2f342a548a625dbac22172b1243
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-03-30 18:36:53 +0300

    Update 2161-partition-array-according-to-given-pivot.py

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
@@ -1,37 +1,4 @@
-#first approach
 class Solution:
-    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        
-        result = [None] * len(nums)
-        
-        left_count = 0
-        right_count = 0
-        
-        for num in nums:
-            if num < pivot:
-                left_count += 1
-            elif num > pivot:
-                right_count += 1
-                
-        left = 0
-        right = len(nums) - right_count
-        
-        p_index = left_count
-        for num in nums:
-            
-            if num < pivot:
-                result[left]  = num
-                left += 1
-            elif num > pivot:
-                result[right] = num
-                right += 1
-            else:
-                result[p_index] = num 
-                p_index += 1
-        return result
-
-    #second approach
-    class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
         
         lessthan = []
