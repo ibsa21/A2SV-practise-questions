ea244739ddc2d9fc46b77c146b16a195b04e5bdb
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-04-13 17:59:13 +0300

    Time: 110 ms (78.36%), Space: 15.5 MB (73.98%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,30 +1,44 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         
-        count_course = [0] * numCourses
+        """
+        steps:
+        1. find number of courses that has prerequisites - > count_course
+        2. add course and it's prerequisites in dictionary 
+        3. add courses that does not have prerequisites in queue
+        4. do bfs for courses that are in queue
+        """
+        #number of prerequisites for each courses
+        no_preq = [0] * numCourses
         
+        #course and it's preq
         preq_course = defaultdict(set)
         
-        for course, req in prerequisites:
-            count_course[course] += 1
-            preq_course[req].add(course)
+        for course, preq in prerequisites:
+            no_preq[course] += 1
+            preq_course[preq].add(course)
+            
         
+        #add course index  that does not have preq
         queue = deque()
         
         for i in range(numCourses):
-            if count_course[i]==0:
+            if no_preq[i]==0:
                 queue.append(i)
+        
         
         course_taken = 0
         while queue:
             course = queue.popleft()
             course_taken += 1
             
-            for courses in preq_course[course]:
-                count_course[courses] -= 1
+            for course_preq in preq_course[course]:
+                no_preq[course_preq] -= 1
                 
-                if count_course[courses] == 0:
-                    queue.append(courses)
+                if no_preq[course_preq]==0:
+                    queue.append(course_preq)
+            
+            
         
         return course_taken == numCourses
-            
+        
