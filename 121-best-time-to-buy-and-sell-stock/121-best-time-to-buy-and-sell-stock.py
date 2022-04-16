c705ddd277863c691025d184c894df849e1d7db6
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-04-16 23:04:23 +0300

    Time: 1203 ms (69.43%), Space: 25 MB (87.45%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,16 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+                
         
-        stack = []
+    
+        #dynamic programming approach
+        buy_stock, max_profit = float(inf), 0
         
-        max_profit = 0
         for i in range(len(prices)):
-            while stack and prices[i] <= stack[-1]:
-                if len(stack) > 1:
-                    max_profit = max(max_profit, abs(stack[0] - stack.pop()))
-                else:
-                    stack.pop()
-            stack.append(prices[i])
-            max_profit = max(max_profit, abs(prices[i]- stack[0]))
+            buy_stock = min(prices[i], buy_stock)
+            max_profit = max(max_profit, abs(prices[i] - buy_stock))
         
         return max_profit
