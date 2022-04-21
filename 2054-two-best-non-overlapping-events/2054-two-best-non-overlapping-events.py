456294818a913f9c09e55a5e1c3bf9f8c4269d2f
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-04-21 11:07:16 +0300

    Time: 2284 ms (74.41%), Space: 57.3 MB (65.88%) - LeetHub

--- 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.py
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.py
@@ -4,20 +4,17 @@ class Solution:
         #step - 1: sort the events based on starting time of events
         events.sort()
         
-
         curr_time_max, global_max_time = 0, 0
         heap = []
         
         for curr_interval in events:
             
             while heap:
-                max_end_time, time_value = heappop(heap)
                 
-                if max_end_time >= curr_interval[0]:
-                    heappush(heap, (max_end_time, time_value))
+                if heap[0][0] < curr_interval[0]:
+                    curr_time_max = max(curr_time_max, heappop(heap)[1])
+                else:
                     break
-                
-                curr_time_max = max(curr_time_max, time_value)
             
             global_max_time = max(global_max_time, curr_time_max + curr_interval[2])
             heappush(heap, (curr_interval[1], curr_interval[2]))
