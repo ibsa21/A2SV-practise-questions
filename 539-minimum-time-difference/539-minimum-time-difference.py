3e16dc4b02e3e6275d1ca387a03fc9b92630cc5b
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-05-09 17:19:22 +0300

    Time: 88 ms (66.03%), Space: 17.2 MB (36.73%) - LeetHub

--- 539-minimum-time-difference/539-minimum-time-difference.py
+++ 539-minimum-time-difference/539-minimum-time-difference.py
@@ -5,9 +5,7 @@ class Solution:
         
         min_difference = 3600
         for i in range(len(timePoints)-1):
-            
             min_difference = min(min_difference, timePoints[i+1]-timePoints[i])
-        
         min_difference  = min(min_difference, abs(timePoints[len(timePoints)-1]-(1440 + timePoints[0])))
         return min_difference
             
