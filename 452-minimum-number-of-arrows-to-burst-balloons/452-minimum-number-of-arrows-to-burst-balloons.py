b29177403f7b44ac8e5f24226041f271ebc8c560
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-05-11 15:23:03 +0300

    Time: 1528 ms (68.06%), Space: 50.5 MB (99.53%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.py
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.py
@@ -1,10 +1,13 @@
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
-        points.sort(key = lambda x:x[1])
-        ans,i = 0,0
-        while i < len(points):
-            x_start, x_end = points[i]
-            while i < len(points) and  points[i][0] <= x_end:
-                i += 1
-            ans += 1
+        for i in range(len(points)):
+            points[i][0], points[i][1] = points[i][1], points[i][0]
+        heapq.heapify(points)
+        ans = 0
+        while points:
+            x_end, x_start = heapq.heappop(points)
+            
+            while points and points[0][1] <= x_end:
+                heapq.heappop(points)
+            ans +=1
         return ans
