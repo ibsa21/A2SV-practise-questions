d8502a7af71808e6b582cb3c3abd0019cd376870
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-05-13 18:14:00 +0300

    Time: 50 ms (26.90%), Space: 13.8 MB (77.66%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.py
+++ 165-compare-version-numbers/165-compare-version-numbers.py
@@ -3,13 +3,7 @@ class Solution:
         v1 = list(map(int, version1.split('.')))
         v2 = list(map(int, version2.split('.')))
         
-        diff = abs(len(v1) - len(v2))
-        if len(v1) > len(v2):v2.extend([0] * diff)
-        elif len(v1) < len(v2):v1.extend([0] * diff)
-            
-        for i in range(min(len(v1), len(v2))):
-            if v1[i] > v2[i]:
-                return 1
-            elif v1[i] < v2[i]:
-                return -1
+        for v_one, v_two in zip_longest(v1, v2, fillvalue = 0):
+            if v_one > v_two:return 1
+            elif v_two > v_one:return -1
         return 0
