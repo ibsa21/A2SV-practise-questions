75fbea8f1dba9d1c9ce8358cd43ce466df97a88e
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-05-19 15:41:51 +0300

    Time: 44 ms (83.04%), Space: 14.3 MB (24.55%) - LeetHub

---
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+                
+        pivot_index = -1
+        
+        def search_range(low, high, k_target):
+            nonlocal pivot_index
+            
+            if low > high:
+                return
+            
+            mid = low + (high-low)//2
+            if k_target < nums[mid]:
+                search_range(mid + 1, high, k_target)
+            elif k_target > nums[mid]:
+                pivot_index = mid
+                search_range(low, mid-1, k_target)
+                
+        search_range(1, len(nums)-1, nums[0])
+        return nums[0] if pivot_index == -1 else nums[pivot_index]
