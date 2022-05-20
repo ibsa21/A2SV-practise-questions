6dd8990115dcfd37082eb1de16d37799d247edd4
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-05-20 15:34:35 +0300

    Time: 51 ms (64.80%), Space: 14.2 MB (11.07%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.py
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -5,15 +5,11 @@ class Solution:
         memo = {}
         def dp(i, j):
             nonlocal result
-            if i >  n or j > m:
-                return 0
+            if i >  n or j > m or grid[i][j]==1:return 0
             
             if (i, j) in memo:return memo[(i, j)]
+            if i == n and j==m:return 1
             
-            if grid[i][j] == 1:
-                return 0
-            if i == n and j==m:
-                 return 1
             memo[(i, j)] = dp(i + 1, j) + dp(i, j + 1)
             return memo[(i,j)]
         return dp(0, 0)
