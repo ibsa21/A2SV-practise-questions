49251c74d6d7374357808ea3127ae9fa78aa48d9
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-06-03 20:27:43 +0300

    Time: 1336 ms (31.35%), Space: 77 MB (8.65%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.py
+++ 41-first-missing-positive/41-first-missing-positive.py
@@ -1,26 +1,7 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        nums.append(0)
-        nums.sort() 
-        
-        isposivite = lambda x, y: x>=0 and y >=0
-        for i in range(1, len(nums)):
-            if isposivite(nums[i], nums[i-1]) and nums[i] - nums[i-1] > 1:
-                return nums[i-1] + 1
-        
-        return nums[-1] + 1
-        
-#         if max(nums) <= 0:return 1
-        
-#         pos_list = set()
-#         min_posnumber = float(inf)
-        
-#         for num in nums:
-#             if num > 0:pos_list.add(num)
-        
-#         for num in nums:
-#             if num -1 > 0 and num-1 not in pos_list:
-#                 min_posnumber = min(min_posnumber, num-1)
-        
-#         if min(pos_list) > 1: return 1
-#         return min_posnumber if min_posnumber != float(inf) else max(pos_list) + 1
+        hashset = Counter(nums)
+        max_number = max(nums)
+        if max_number < 1:return 1
+        for i in range(1, max_number + 2):
+            if  i not in hashset :return i
