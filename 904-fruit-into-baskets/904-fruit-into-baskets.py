a0c9051001cbdbd51eab828ae8380ca27cbfe3b6
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-06-12 19:17:23 +0300

    Time: 2007 ms (5.36%), Space: 20.1 MB (87.77%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.py
+++ 904-fruit-into-baskets/904-fruit-into-baskets.py
@@ -2,20 +2,16 @@ class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
 #         [1,0,1,4,1,4,1,2,3]
         l,r = 0, 0
-        picked = set()
         my_dict = defaultdict(int)
         ans = 0
         
         while r < len(fruits):
-            
-            picked.add(fruits[r])
             my_dict[fruits[r]] +=1 
-            while len(picked) > 2:
+            while len(my_dict.keys()) > 2:
                 my_dict[fruits[l]] -= 1
                 if my_dict[fruits[l]] < 1:
                     del my_dict[fruits[l]]
                 l += 1
-                picked = set(my_dict.keys())
             ans = max(ans, r - l + 1)
             r += 1
         
