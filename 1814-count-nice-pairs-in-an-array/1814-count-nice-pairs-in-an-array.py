f79dc0492fa4a791503cea9f61e6096c5b623fe1
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-06-17 23:04:46 +0300

    Time: 792 ms (88.94%), Space: 24.5 MB (26.54%) - LeetHub

--- 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.py
+++ 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.py
@@ -1,9 +1,8 @@
 class Solution:
     def countNicePairs(self, nums: List[int]) -> int:
-        rev = defaultdict(int)
         for idx, num in enumerate(nums):
-            rev[num] = int(str(num)[::-1])
-            nums[idx] = num - rev[num]
+            rev = int(str(num)[::-1])
+            nums[idx] = num - rev
         counter = Counter(nums)
         num_good_pairs = 0
         for key in counter:
