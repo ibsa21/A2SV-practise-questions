a5c871c2708e34018bdd50003ad6a45258183139
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-06-17 20:31:15 +0300

    Time: 79 ms (30.14%), Space: 14.5 MB (8.84%) - LeetHub

---
+++ 968-binary-tree-cameras/968-binary-tree-cameras.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minCameraCover(self, root: Optional[TreeNode]) -> int:
+        
+        #gready + depth first search
+        visited = {None}
+        
+        childVisited = lambda parent: parent.left not in visited or parent.right not in visited
+        isNodeRoot = lambda node, parent: not parent and node not in visited 
+        minimum_camera = 0
+        def dfs(node, parent):
+            nonlocal minimum_camera
+            if not node: return
+
+            dfs(node.left, node)
+            dfs(node.right, node)
+
+            if isNodeRoot(node, parent) or childVisited(node):
+                minimum_camera += 1
+                visited.update({node, parent, node.left})
+            
+            return minimum_camera
+        return dfs(root, None)
+        # return minimum_camera
