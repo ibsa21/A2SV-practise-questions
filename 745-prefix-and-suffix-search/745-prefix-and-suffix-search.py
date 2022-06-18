503d6622c73e1435f664106dbb6ebf9fb335d8c6
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-06-18 17:56:14 +0300

    Time: 2105 ms (43.43%), Space: 77.8 MB (14.99%) - LeetHub

--- 745-prefix-and-suffix-search/745-prefix-and-suffix-search.py
+++ 745-prefix-and-suffix-search/745-prefix-and-suffix-search.py
@@ -1,11 +1,9 @@
 class TrieNode:
-    
     def __init__(self):
         self.children = {}
         self.end_index = -1
 
 class WordFilter:
-
     def __init__(self, words: List[str]):
         self.root = TrieNode()
         
@@ -21,7 +19,6 @@ class WordFilter:
         for char in word:
             if char not in cur.children:
                 cur.children[char] = TrieNode()
-
             cur.end_index  = idx
             cur = cur.children[char]
 
@@ -36,8 +33,10 @@ class WordFilter:
     
     def f(self, prefix, suffix):
         
+        #search the suffix
         cur = self.search(suffix[::-1], self.root)
         if not cur or "#" not in cur.children: return -1
         
+        # search for the prefix
         cur  = self.search(prefix, cur.children["#"])
         return -1 if not cur else cur.end_index
