6a5c87a639d91de7d2ca0a7edd9f4d12e23d47a5
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-06-20 20:56:49 +0300

    Time: 968 ms (8.38%), Space: 21.6 MB (5.12%) - LeetHub

---
+++ 820-short-encoding-of-words/820-short-encoding-of-words.py
@@ -0,0 +1,44 @@
+class TrieNode:
+    def __init__(self, word = ''):
+        self.children ={}
+        self.end_suffix = False
+    
+class Solution:
+    def minimumLengthEncoding(self, words: List[str]) -> int:
+        
+        #let's build a trie node
+        root = TrieNode()
+        
+        def insert(word):
+            cur_root = root
+            for char in word:
+                if char not in cur_root.children:
+                    cur_root.children[char]  = TrieNode(word)
+                
+                cur_root = cur_root.children[char]
+            cur_root.end_suffix = True
+            return
+        
+        def search(word):
+            cur_root = root
+            for char in word:
+                if char not in cur_root.children:
+                    return False
+                cur_root= cur_root.children[char]
+            return cur_root.end_suffix
+                
+        shortest_encoding_length  = 0
+        
+        #let's sort the words based on their length
+        words = sorted(words, key =  lambda x: len(x))
+        
+        #build trie node
+        for word in words[::-1]:
+            
+            if not search(word):
+                shortest_encoding_length += (len(word) + 1)
+            cur_suffix = ''
+            for idx, char in enumerate(word[::-1]):
+                cur_suffix = char + cur_suffix
+                insert(cur_suffix)
+        return shortest_encoding_length
