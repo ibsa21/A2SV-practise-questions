851c4eecad8dd793d62cea116c3b4bd8cbe78f71
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-06-26 17:13:03 +0300

    Time: 214 ms (33.47%), Space: 21.7 MB (32.37%) - LeetHub

--- 386-lexicographical-numbers/386-lexicographical-numbers.py
+++ 386-lexicographical-numbers/386-lexicographical-numbers.py
@@ -1,5 +1,22 @@
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
+        # """time complexity: O(n)
+        #    space complexity: O(n)
+        # """
+        # nums_dict = defaultdict(list)
+        # for num in range(1, n + 1):
+        #     first_char = str(num)[0]
+        #     nums_dict[first_char].append(num)
+        # nums = []
+        # for key in nums_dict:
+        #     for num in nums_dict[key]:
+        #         nums.append(num)
+        # return nums
+    
+        """
+            time complexity: O(nlogn)
+            space complexity: O(n)
+        """
         nums = [str(num) for num in range(1, n + 1)]
         nums.sort()
-        return [int(num) for num in nums]
+        return list(map(int, nums))
