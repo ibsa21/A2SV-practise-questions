b8895261f0997f51ea694538156e299562bc52a7
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-06-26 17:31:21 +0300

    Time: 362 ms (13.50%), Space: 29.7 MB (5.10%) - LeetHub

--- 386-lexicographical-numbers/386-lexicographical-numbers.py
+++ 386-lexicographical-numbers/386-lexicographical-numbers.py
@@ -1,18 +1,32 @@
+class TrieNode:
+    def __init__(self, digit:str = ''):
+        self.children:Dict[str, Dict] = {}
+        self.digit = digit
+        self.is_end:bool = False
+        
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
-        # """time complexity: O(n)
-        #    space complexity: O(n)
-        # """
-        # nums_dict = defaultdict(list)
-        # for num in range(1, n + 1):
-        #     first_char = str(num)[0]
-        #     nums_dict[first_char].append(num)
-        # nums = []
-        # for key in nums_dict:
-        #     for num in nums_dict[key]:
-        #         nums.append(num)
-        # return nums
-    
+        root = TrieNode()
+        
+        def insert(alphanum:str)->None:
+            cur_root:TrieNode = root
+            for digit in alphanum:
+                if digit not in cur_root.children:
+                    cur_root.children[digit] = TrieNode(digit)
+                cur_root = cur_root.children[digit]
+            cur_root.is_end = True
+        
+        for num in range(1, n + 1):
+            insert(str(num))
+        
+        ans = []
+        def dfs(node, cur_num):
+            if cur_num != '0':ans.append(int(cur_num))
+            for child in node.children:
+                dfs(node.children[child], cur_num + child)
+        dfs(root, '0')
+        return ans
+            
         """
             time complexity: O(nlogn)
             space complexity: O(n)
