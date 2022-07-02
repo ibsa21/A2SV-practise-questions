4a8b8a6b959e4451b2016d9357c764561640a33c
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-07-02 07:53:58 +0300

    Time: 456 ms (50.78%), Space: 26.7 MB (77.43%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
@@ -3,16 +3,12 @@ class Solution:
         horizontalCuts.sort()
         verticalCuts.sort()
         
-        max_column = max(0, horizontalCuts[0])
-        for idx, row in enumerate(horizontalCuts):
-            if idx != 0:
-                max_column = max(max_column, abs(row - horizontalCuts[idx-1]))
-        max_column = max(max_column, abs(h-horizontalCuts[-1]))
-        
-        max_row = max(0, verticalCuts[0])
-        for idx, col in enumerate(verticalCuts):
-            if idx != 0:
-                max_row = max(max_row, abs(col - verticalCuts[idx-1]))
-                
-        max_row = max(max_row, abs(w- verticalCuts[-1]))
-        return (max_column * max_row) % (pow(10, 9) + 7)
+        def get_max_diff(arr, h_w):
+            max_column = max(0, arr[0])
+            for idx, row in enumerate(arr):
+                if idx != 0:
+                    max_column = max(max_column, abs(row - arr[idx-1]))
+            max_column = max(max_column, abs(h_w-arr[-1]))
+            return max_column
+
+        return (get_max_diff(horizontalCuts, h) * get_max_diff(verticalCuts, w)) % (pow(10, 9) + 7)
