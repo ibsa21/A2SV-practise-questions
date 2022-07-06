878cbbeb7b6b180b25e27677d6c4fa6e7cccb98f
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-07-06 13:17:56 +0300

    Time: 357 ms (87.37%), Space: 33.5 MB (9.11%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -6,18 +6,22 @@ class Solution:
         '''
         nums_set = set(nums)            # type; Set
         counted_nums = set()            # type: Set
-        
+        number_operation = 0
         def count_less_num(num:int)->int:
+            nonlocal number_operation
             count = 1       # type: int
-            while num -1 in nums_set:
+            while num -1 in nums_set and num -1 not in counted_nums:
+                number_operation += 1
                 num -= 1
                 counted_nums.add(num)
                 count += 1
             return count
         
         def count_greater_num(num:int) -> int:
+            nonlocal number_operation
             count = 1       # type: int
-            while num + 1 in nums_set:
+            while num + 1 in nums_set and num + 1  not in counted_nums:
+                number_operation += 1
                 num += 1
                 counted_nums.add(num)
                 count += 1
@@ -27,4 +31,5 @@ class Solution:
         for num in nums:
             if num not in counted_nums: 
                 longest_sequence = max(longest_sequence, count_less_num(num) + count_greater_num(num)-1)
+        print(number_operation)
         return longest_sequence
