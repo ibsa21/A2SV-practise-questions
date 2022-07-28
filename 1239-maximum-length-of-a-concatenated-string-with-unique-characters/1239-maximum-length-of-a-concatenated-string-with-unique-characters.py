2bca1c2c986d58c2efbf8e65c29a57f1c7f17163
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-07-28 14:03:43 +0300

    Time: 3847 ms (5.11%), Space: 13.8 MB (89.51%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -5,7 +5,7 @@ class Solution:
         maximum_length = 0
         for r in range(1, len(arr) + 1):
             for comb in combinations(arr, r):
-                length_char = sum([len(comb[i]) for i in range(len(comb))])
+                length_char = sum(len(substring) for substring in comb)
                 if len(set(''.join(comb))) == length_char:
                     maximum_length = max(maximum_length, length_char)
         return maximum_length
