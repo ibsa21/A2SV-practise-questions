b905988fe06e813f890f7b3200383aeb3ade79e1
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-08-01 11:25:49 +0300

    Time: 45 ms (63.14%), Space: 14.3 MB (6.77%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,10 +1,15 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        memo = {}
-        def dp(i, j):
-            if i > m-1 or j > n-1:return 0
-            if i==m-1 and j==n-1:return 1
-            if (i, j) in memo:return memo[(i, j)]
-            memo[(i ,j)] = dp(i + 1, j) + dp(i, j + 1)
-            return memo[(i, j)]
-        return dp(0, 0)
+        
+        inbound = lambda r, c: 0<=r<m and 0<=c<n
+        memo  = {}
+        def dfs(l, r):
+            
+            if inbound(l, r):
+                if l==m-1 and r==n-1: return 1
+                if (l, r) in memo: return memo[(l, r)]
+                
+                memo[(l, r)] = dfs(l + 1, r) + dfs(l, r + 1)
+                return memo[(l, r)]
+            else: return 0
+        return dfs(0, 0)
