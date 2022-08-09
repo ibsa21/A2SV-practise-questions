8927851ad102dac7d07ebd91b92d9d077c40bfc6
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-08-09 11:09:07 +0300

    Time: 601 ms (54.30%), Space: 14.1 MB (55.85%) - LeetHub

--- 368-largest-divisible-subset/368-largest-divisible-subset.py
+++ 368-largest-divisible-subset/368-largest-divisible-subset.py
@@ -1,7 +1,10 @@
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        '''
+            Time complexity: O(N**2)
+            Space complexity: O(N)
+        '''
         nums.sort()
-        
         max_subset = 1
         dp = [1] * len(nums)
         for i in range(1, len(nums)):
@@ -10,12 +13,12 @@ class Solution:
                     dp[i] = max(dp[i], dp[j] + 1)
             max_subset = max(max_subset, dp[i])
         
-        prev_num = None
         res = []
-        for i in range(len(nums)-1, -1, -1):
+        i, prev_num = len(nums)-1, None
+        while max_subset != 0:
             if dp[i]==max_subset  and (prev_num is None or prev_num % nums[i] == 0):
                 res.append(nums[i])
                 prev_num = nums[i]
                 max_subset -= 1
-            if max_subset == 0: return res
-        
+            i -= 1
+        return res
