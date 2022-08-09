8480aa0abb1e1d693343ea82634ddc6173b17db0
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-08-09 17:01:38 +0300

    Time: 81 ms (15.11%), Space: 14 MB (58.61%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,16 +1,16 @@
 from itertools import permutations
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        ans = []
-        def dfs(n,path):
-            if len(path) == len(nums):
-                ans.append(path)
+        
+        def backtrack(nums, path, result):
+            if not nums:
+                result.append(path)
                 return
-            for i in nums:
-                if i not in path:
-                    # path += [i]
-                    dfs(i,path+[i])
-                    # path.pop()
-        for i in nums:
-            dfs(i,[i])
-        return ans
+            for i in range(len(nums)):
+                backtrack(nums[:i] + nums[i + 1:], path + [nums[i]], result)
+        
+        result = []
+        backtrack(nums, [], result)
+        return result
+            
+        
