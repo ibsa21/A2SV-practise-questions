cc4c6045afde18c27b19d3b691b34e085225450a
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-08-10 14:48:04 +0300

    Time: 569 ms (94.40%), Space: 27.1 MB (72.70%) - LeetHub

---
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -0,0 +1,17 @@
+from bisect import bisect_left
+class Solution:
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        
+        jobs = [(startTime[i], endTime[i], profit[i]) for i in range(len(startTime))]
+        jobs.sort()
+        
+        startTime, dp = [], []
+        for start, end, profit in jobs:
+            startTime.append(start)
+            dp.append(profit)
+            
+        for i in range(len(jobs)-2, -1 , -1):
+            idx = bisect_left(startTime, jobs[i][1])
+            prev_value = dp[idx] if idx < len(jobs) else 0
+            dp[i] = max(dp[i + 1], prev_value + dp[i])
+        return dp[0]
