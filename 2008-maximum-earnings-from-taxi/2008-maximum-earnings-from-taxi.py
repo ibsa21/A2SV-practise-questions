da977f8946ba45bfa8dab5fc0c996c6883b8bb18
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-08-10 11:51:52 +0300

    Time: 2318 ms (79.00%), Space: 33.3 MB (88.36%) - LeetHub

---
+++ 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.py
@@ -0,0 +1,13 @@
+from bisect import bisect_left
+class Solution:
+    def maxTaxiEarnings(self, n: int, rides: List[List[int]]) -> int:
+        
+        rides.sort()
+        dp = [end-start+tip for start, end, tip in rides]
+        startingPoint = [ride[0] for ride in rides]
+        
+        for i in range(len(rides)-2, -1, -1):
+            idx = bisect_left(startingPoint, rides[i][1])
+            maxValue = dp[idx] if idx < len(rides) else 0
+            dp[i] = max(dp[i + 1], dp[i] + maxValue)
+        return dp[0]
