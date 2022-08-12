5a861e043ce64b0dc602427ae2360e1bdfe7ce06
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-08-12 12:27:07 +0300

    Time: 55 ms (32.00%), Space: 13.8 MB (68.27%) - LeetHub

---
+++ 50-powx-n/50-powx-n.py
@@ -0,0 +1,17 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        
+        def solve(x, n):
+            if n==0: return 1
+            
+            if n  < 0:
+                n  = abs(n)
+                x = 1/x
+            
+            a = solve(x, n//2)
+            if n % 2==0:
+                return a * a
+            else:
+                return x * (a * a)
+        return solve(x, n)
+            
