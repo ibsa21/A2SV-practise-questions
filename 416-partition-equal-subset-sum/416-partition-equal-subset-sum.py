7133dd0dba33dffe1bbfdd824e27913d9b7fa021
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-08-20 00:14:04 +0300

    Time: 3887 ms (21.46%), Space: 570 MB (5.01%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -1,19 +1,19 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         
-        
+        # return False
         memo = {}
         def dfs(idx, cur_sum):
-            if cur_sum==0: return True
-            if idx >= len(nums) or cur_sum < 0: return False
+            
+            if cur_sum == total_sum/2: return True
+            if idx >= len(nums): return False
             
             if (idx, cur_sum) in memo: return memo[(idx, cur_sum)]
             
-            memo[(idx, cur_sum)] = dfs(idx + 1, cur_sum) or dfs(idx + 1, cur_sum - nums[idx])
+            result = dfs(idx + 1, cur_sum) or dfs(idx + 1, cur_sum + nums[idx])
+            memo[(idx, cur_sum)]= result
             return memo[(idx, cur_sum)]
             
         total_sum = sum(nums)
-        if total_sum %2==1: return False
-        return dfs(0, total_sum//2)
-            
-        
+        if total_sum % 2 != 0: return False
+        return dfs(0, 0)
