a1b2ff4b6b4b006d317aa85bc3a7cbb3d30c628e
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-08-25 21:36:18 +0300

    Time: 2592 ms (18.15%), Space: 59.7 MB (87.66%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.py
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.py
@@ -1,26 +1,19 @@
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
-        #heap approach
-        for i in range(len(points)):
-            points[i][0], points[i][1] = points[i][1], points[i][0]
-        heapq.heapify(points)
-        ans = 0
-        while points:
-            x_end, x_start = heapq.heappop(points)
-            
-            while points and points[0][1] <= x_end:
-                heapq.heappop(points)
-            ans +=1
-        return ans
-    
-        #sotring approach
-class Solution:
-    def findMinArrowShots(self, points: List[List[int]]) -> int:
-        points.sort(key = lambda x:x[1])
-        ans,i = 0,0
-        while i < len(points):
-            x_start, x_end = points[i]
-            while i < len(points) and  points[i][0] <= x_end:
-                i += 1
-            ans += 1
+        #your code if better
+        #you could've optimized your solution
+        
+        if len(points) <2: return len(points)
+        points.sort()  #O(nlogn)
+        largeBallons = points[0][1]
+        ans = 1
+        
+        for i in range(1, len(points)):   #O(N)
+            if points[i][0]<=largeBallons:
+                largeBallons = min(points[i][1],largeBallons)
+                continue
+                
+            largeBallons=points[i][1]
+            ans +=1 
+
         return ans
