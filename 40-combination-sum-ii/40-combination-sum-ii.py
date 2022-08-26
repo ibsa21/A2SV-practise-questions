00e3dab67ab4b67ec51fb4ff6d0e7630a7ae8d25
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-08-26 22:31:38 +0300

    Time: 177 ms (13.54%), Space: 13.4 MB (81.83%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.py
+++ 40-combination-sum-ii/40-combination-sum-ii.py
@@ -4,48 +4,24 @@ class Solution(object):
         :type candidates: List[int]
         :type target: int
         :rtype: List[List[int]]
-        """          
-        
+        """
         candidates.sort()
         global_ans = []
         def backtrack(idx, curr_sum, local_ans, target):
-            # print(curr_sum, target, idx)
-            if  curr_sum > target: return
+            if curr_sum > target: return
             if curr_sum == target:
                 global_ans.append(local_ans[::])
                 return
-
-            for i in range(idx, len(candidates)):
-                if i != idx and candidates[i] == candidates[i-1]: continue
-
-                local_ans.append(candidates[i])
-                backtrack(i+1,curr_sum+candidates[i], local_ans, target)
-                local_ans.pop()
-
+            
+            i  = idx
+            while i < len(candidates):
+                while i >idx and i < len(candidates)  and candidates[i]==candidates[i-1]:
+                    i += 1
+                if i < len(candidates):
+                    local_ans.append(candidates[i])
+                    backtrack(i + 1, curr_sum + candidates[i], local_ans, target)
+                    local_ans.pop()
+                i += 1
+                
         backtrack(0, 0, [], target)
         return global_ans
-
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
