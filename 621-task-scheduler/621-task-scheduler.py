3692204d8ff6c93a4361edf5c5af27c8c38a57f4
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-08-28 14:56:49 +0300

    Time: 451 ms (92.97%), Space: 14.3 MB (91.10%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.py
+++ 621-task-scheduler/621-task-scheduler.py
@@ -1,27 +1,19 @@
-import math
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
+        frequency = [-values for values in collections.Counter(tasks).values()]
+       
+        # maxNumber = max(frequency)
+        heapq.heapify(frequency)
+        maxNumber = -frequency[0]
         
-        if n==0: return len(tasks)
-        #["A","A","A","B","B","B"], n = 2
-        #find the frequency of each task
-        
-        freq_counter = defaultdict(int)
-        for task in tasks:
-            freq_counter[task] += 1
-        
-        #A-3 and B-3
-            
-        #sorting from the largest to smallest (most frequent to the least frequnt)
-        tasks_sorted_bf = [(value, key) for key, value in freq_counter.items()]
-        tasks_sorted_bf.sort(reverse =True)
-        
-        #each time I'do tasks I'll reduce the frequency of the task
-        number_units = 0
-        for idx, (value, task) in enumerate(tasks_sorted_bf):
-            last_position = ((value-1) * (n + 1)) + idx + 1
-            number_units = max(last_position, number_units)
-            
-        return max(len(tasks), number_units)      
-        
+        final = (maxNumber -1) * (n+1)
+        c = 0
+        for v in frequency:
+            if -v == maxNumber:
+                c += 1
+                
+        return max(final + c,len(tasks))
+        #tasks
+    
+                
         
