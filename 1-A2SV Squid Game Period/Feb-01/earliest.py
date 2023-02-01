d5120c2ae1d2ce149402ec51efd5cc6393f4cde2
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2023-02-02 00:05:10 +0300

    Update earliest.py

--- 1-A2SV Squid Game Period/Feb-01/earliest.py
+++ 1-A2SV Squid Game Period/Feb-01/earliest.py
@@ -1,21 +1,38 @@
 class Solution:
     def earliestFullBloom(self, floweringTime: List[int], growthTime: List[int]) -> int:
-        sortedPlants = sorted(zip(growthTime,floweringTime), reverse = True)
-   
+        sortedPlants = sorted(zip(floweringTime, growthTime), reverse = True)
+
         totalGrowthTime = 0
         availableTimeToPlant = 0
-        
-        for growPeriod, flowerPeriod in sortedPlants:
+
+        for flowerPeriod, growPeriod in sortedPlants:
             if availableTimeToPlant >= flowerPeriod:
+                totalGrowthTime += growPeriod
                 availableTimeToPlant -= flowerPeriod
-                
-                if availableTimeToPlant < growPeriod:
-                    totalGrowthTime += growPeriod - availableTimeToPlant 
-                    availableTimeToPlant = growPeriod
-                    
             else:
-                totalGrowthTime += flowerPeriod
-                totalGrowthTime += growPeriod - availableTimeToPlant 
+                totalGrowthTime += flowerPeriod + growPeriod
                 availableTimeToPlant = growPeriod
-        
+
         return totalGrowthTime
+    
+# class Solution:
+#     def earliestFullBloom(self, floweringTime: List[int], growthTime: List[int]) -> int:
+#         sortedPlants = sorted(zip(growthTime,floweringTime), reverse = True)
+   
+#         totalGrowthTime = 0
+#         availableTimeToPlant = 0
+        
+#         for growPeriod, flowerPeriod in sortedPlants:
+#             if availableTimeToPlant >= flowerPeriod:
+#                 availableTimeToPlant -= flowerPeriod
+                
+#                 if availableTimeToPlant < growPeriod:
+#                     totalGrowthTime += growPeriod - availableTimeToPlant 
+#                     availableTimeToPlant = growPeriod
+                    
+#             else:
+#                 totalGrowthTime += flowerPeriod
+#                 totalGrowthTime += growPeriod - availableTimeToPlant 
+#                 availableTimeToPlant = growPeriod
+        
+#         return totalGrowthTime
