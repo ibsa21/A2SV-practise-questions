8c67557dbfdfda90dc6cf32abb6fdfd6d8ec9133
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2023-03-21 18:23:07 +0300

    Time: 310 ms (37.63%), Space: 16.8 MB (10.69%) - LeetHub

---
+++ 0490-the-maze/0490-the-maze.py
@@ -0,0 +1,44 @@
+class Solution:
+    def hasPath(self, maze: List[List[int]], start: List[int], destination: List[int]) -> bool:
+        visited = set()
+        rows  = len(maze)
+        cols = len(maze[0])
+        
+        dirs = [(-1,0),(1,0),(0,1),(0,-1)]
+        
+        inbound = lambda row,col: 0<=row<rows and 0<=col<cols
+        
+        def move(r,c):
+            nextPosition = set()
+            for move_x, move_y in dirs:
+                row,col = r,c
+                while inbound(row,col) and  maze[row][col] != 1:
+                    row += move_x
+                    col += move_y
+
+                if inbound(row,col) and maze[row][col] != 1:
+                    nextPosition.add((row,col))
+                nextPosition.add((row-move_x, col-move_y))
+            return nextPosition
+        
+        # print(inbound(0,4))
+        print(move(0,4))
+        # for x,y in dirs:
+            # print(move(0,4,x,y))
+        
+        def dfs(row,col):
+            
+            # print((row,col), destination)
+            if [row, col] == destination:
+                print("hello")
+                return True
+            
+            visited.add((row,col))
+            nextPositions = move(row,col)
+            # print(nextPositions)
+            for new_row, new_col in nextPositions:
+                if (new_row, new_col) not in visited:
+                    if dfs(new_row, new_col):
+                        return True
+            return False
+        return dfs(*start)
